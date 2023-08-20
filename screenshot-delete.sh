46de9662f19fc44ca88bd06e7275bc68d6973c43
Darcy London <darcy.london@icloud.com>
2023-08-21 08:18:39 +1000

    Added status, needs work though
    Signed-off-by: Darcy London <darcy.london@icloud.com>

--- screenshot-delete.sh
+++ screenshot-delete.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# go to the dekstop and delete the stuff
 cd ~/Desktop
-rm -r *.png
-rm -r *.jpg
+rm *.png
+rm *.jpg
+
+# print out the status
+status=$?
+ 
+if test $status -eq 0
+then
+	echo "Files deleted!"
+else
+	echo "Failed!"
+fi
