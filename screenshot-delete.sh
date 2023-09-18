252fa74ba7c2901ea77c6955264b39170229abc6
dr-london <darcy.london@icloud.com>
2023-09-19 09:53:55 +1000

    Added some status text:
    Signed-off-by: dr-london <darcy.london@icloud.com>

--- screenshot-delete.sh
+++ screenshot-delete.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # go to the dekstop and delete the stuff
+echo "---> moving to desktop"
 cd ~/Desktop
+
+echo "---> deleting files..."
 rm *.jpg 2>/dev/null && t=0 || t=1
 rm *.png 2>/dev/null && t=0 || t=1
 
@@ -10,9 +13,9 @@ RETURN=$?
 
 if [ $RETURN -eq 0 ];
 then
-  echo "The script was executed successfuly"
+  echo "---> Done!"
   exit 0
 else
-  echo "The script was NOT executed successfuly and returned the code $RETURN"
+  echo "*** Error: $RETURN"
   exit $RETURN
 fi 
