66dd908fa950fe97097e1d14d0b09c82a6c7ab6e
dr-london <darcy.london@icloud.com>
2023-10-06 14:59:03 +1100

    Updated?
    Signed-off-by: dr-london <darcy.london@icloud.com>

--- screenshot-mover.sh
+++ screenshot-mover.sh
@@ -3,17 +3,16 @@
 # grab the current date, save it in a variable
 currentdate=$(date +"%Y-%m-%d")
 
-
 # create the new directory named after the date
 cd ~/Desktop/Archive
-mkdir "Screenshots ${currentdate}"
-echo "---> creating folder ${currentdate} in ${pwd}..."
+mkdir "Screenshots $currentdate"
+echo "---> creating folder $currentdate in $PWD..."
 
 # move all the junk off the desktop and into the new folder
 cd ~/Desktop
-mv *.png Archive/{currentdate} 2>/dev/null && t=0 || t=1
-mv *.jpg Archive/{currentdate} 2>/dev/null && t=0 || t=1
-mv *.jpeg Archive/{currentdate} 2>/dev/null && t=0 || t=1
+mv *.png Archive/$currentdate 2>/dev/null && t=0 || t=1 ## mv cmd to be fixed here
+mv *.jpg Archive/$currentdate 2>/dev/null && t=0 || t=1
+mv *.jpeg Archive/$currentdate 2>/dev/null && t=0 || t=1
 echo "----> moving files..."
 
 # print out the status
