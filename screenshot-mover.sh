c3e0be1194b5e34290c692181f84ce5cb8d6c5ca
dr-london <darcy.london@icloud.com>
2023-10-23 12:25:39 +1100

    Fixed error with moving the images, removed status exit (see comments)
    Signed-off-by: dr-london <darcy.london@icloud.com>

--- screenshot-mover.sh
+++ screenshot-mover.sh
@@ -10,7 +10,7 @@ mkdir "Screenshots $currentdate" 2>/dev/null
 # check if there's already a dir there
 if [ $? -eq 0 ]
 then
-  echo "---> creating folder $currentdate in $PWD..."
+  echo "---> creating folder Screenshots $currentdate in $PWD..."
 else
   echo "---> dir already created, moving on..."
 fi
@@ -18,17 +18,10 @@ fi
 # move all the junk off the desktop and into the new folder
 echo "----> moving files..."
 cd ~/Desktop
-mv *.png ~/Desktop/Archive/"Screenshots $currentdate" 2>/dev/null
-mv *.jpg ~/Desktop/Archive/"Screenshots $currentdate" 2>/dev/null
+mv *.png Archive/Screenshots\ $currentdate 2>/dev/null
+mv *.jpg Archive/Screenshots\ $currentdate 2>/dev/null
+mv *.jpeg Archive/Screenshots\ $currentdate 2>/dev/null
 
-# print out the status
-RETURN=$?
 
-if [ $RETURN -eq 0 ];
-then
-  echo "-----> done!"
-  exit 0
-else
-  echo "*** Error: $RETURN"
-  exit $RETURN
-fi
+# Removed status as each file type is its own command. If one fails, then it will report exit 1 even if the others work.
+echo "-----> done!"
