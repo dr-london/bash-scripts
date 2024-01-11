fea1e1e81d7e444d113c5cd6da5c22e95bbccaed
dr-london <darcy.london@icloud.com>
2024-01-11 15:24:28 +1100

    Removed bloat
    Signed-off-by: dr-london <darcy.london@icloud.com>

--- ssh-keygen2op.sh
+++ ssh-keygen2op.sh
@@ -1,20 +1,13 @@
 #!/bin/bash
+# generates keys and saves them to 1password.
 
-# specify the name for the key
 key_name="$(date +"%Y-%m-%d") ssh-ed25519 key"
 
-# generate the SSH key pair
 ssh-keygen -t ed25519 -f ~/.ssh/"$key_name"
 
-# check if key generation was successful
 if [ $? -eq 0 ]; then
-    # create an item in 1Password
     uuid=$(op item create --title "$key_name" --category "SSH Key")
-
-    # attach the private key file as a document to the item
-    op item edit $uuid 'private-key='$(cat ~/.ssh/"$key_name") 2>/dev/null && t=0 || t=1
-
-    # securely delete the private key
+    op item edit $uuid private-key=$(cat ~/.ssh/"$key_name") 2>/dev/null && t=0 || t=1
     rm ~/.ssh/"$key_name"
     
     echo "---> ssh key generated and stored in 1Password successfully"
