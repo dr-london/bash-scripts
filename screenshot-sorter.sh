04197864290387273438bd5ad6309fa29a366789
dr-london <darcy.london@icloud.com>
2024-01-31 15:39:34 +1100

    Idea for a new script
    Signed-off-by: dr-london <darcy.london@icloud.com>

---
+++ screenshot-sorter.sh
@@ -0,0 +1,10 @@
+#/bin/bash
+
+# idea:
+# create a script to pull OUT any screenshots 
+# that have been archived by the remover script
+# and put them into a folder to be sorted
+
+# just in case any items that were named for explicit reasons
+# aren't being shoved into the archive and lost.
+
